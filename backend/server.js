42e3be7d0861fe30e9eb946fcc3b3e9e97967280
aishat <suleaishat990@gmail.com>
2022-12-06 17:48:17 -0900

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -26,7 +27,14 @@ app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
-
+//
+//
+const _dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontrnd/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
+//
 //
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
